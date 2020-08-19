372ec5feb759dd64609815f97686fdb55045aea8
ldxzyzx567 <69224615+ldxzyzx567@users.noreply.github.com>
2020-08-19 16:45:01 +0800

    Update day11.java
    4545

--- DAY11/src/dyaa/day11.java
+++ DAY11/src/dyaa/day11.java
@@ -6,8 +6,6 @@ public static void main(String[] args) {
 	System.out.println(234);
 	
 	System.out.println(234);
-        System.out.println(234);
-	System.out.println(234);
 	System.out.println(1452);
 	System.out.println(2);
 
