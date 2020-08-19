fde5e4409edfa9d3acc85ab31e97822028295128
ldxzyzx567 <69224615+ldxzyzx567@users.noreply.github.com>
2020-08-19 16:41:28 +0800

    Update day11.java
    额额

--- DAY11/src/dyaa/day11.java
+++ DAY11/src/dyaa/day11.java
@@ -5,8 +5,9 @@ public static void main(String[] args) {
 
 	System.out.println(234);
 	
-	
-
+	System.out.println(234);
+        System.out.println(234);
+	System.out.println(234);
 	System.out.println(1452);
 	System.out.println(2);
 
